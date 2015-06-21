fc7660600764f6b97cc3f3ced7bce770919f8627
slayterson <slayterson@gmail.com>
2015-06-21 00:07:45 -0700

    Create dc_disp.hpp
    Easier way to merge rcstitles.

---
+++ CreditZ/dc_disp.hpp
@@ -0,0 +1,28 @@
+// DayZ CreditZ
+class RscTitles
+{
+titles[] = {"creditz"};
+ 
+class creditz {
+   idd = -1;
+   movingEnable = 0;
+   duration = 10;
+   fadein = 2;
+   fadeout = 2;
+   name="creditz";
+ 
+   controls[]={"Picture"};
+ 
+   class Picture {
+      x=-0.5; y=-0.5; w=2.0; h=2.0;
+      text="addons\CreditZ\DC1.paa";
+      sizeEx = -1;
+      type=0;
+      idc=-1;
+      style=48;
+      colorBackground[]={0,0,0,0};
+      colorText[]={1,1,1,1};
+      font="Bitstream";
+      };
+   };
+};
